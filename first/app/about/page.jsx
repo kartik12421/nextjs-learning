58dc97a04d0bec7b4663c6cc01e46fa60797659f
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-04-14 15:46:02 +0530

    learning and practicing - 1

---
+++ first/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">hello Buddy...</h1>
+      <ul>
+        <li>Full stack solution</li>
+        <li>Fole based routing</li>
+        <li>Additional features like router from next/navigation</li>
+        <li>Optimized rendering</li>
+      </ul>
+    </div>
+  );
+};
+
+export default page;
